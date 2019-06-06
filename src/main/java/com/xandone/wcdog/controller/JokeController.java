edf2148270d5744b01f69811b9a24b5305b5bfb7
xandone <765478955@qq.com>
2019-06-06 17:32:08 +0800

    跨域

--- src/main/java/com/xandone/wcdog/controller/JokeController.java
+++ src/main/java/com/xandone/wcdog/controller/JokeController.java
@@ -12,10 +12,7 @@ import com.xandone.wcdog.utils.IDUtils;
 import com.xandone.wcdog.utils.SimpleUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Date;
